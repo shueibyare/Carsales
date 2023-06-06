d04c15bf28eb586607ac9ea4be85010f4334c01e
shueib yare <shueibyussuf10@gmail.com>
2023-06-06 23:43:15 +0300

    Projo commit

---
+++ MagariSales/cars/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render,redirect
+from django.urls import reverse
+from . import models
+
+
+# Create your views here.
+def list(request):
+    all_cars = models.Car.objects.all()
+    context = {'all_cars':all_cars}
+
+
+    return render(request,'cars/list.html',context=context)
+
+
+def add(request):
+    if request.POST:
+        brand = request.POST['brand']
+        year = int(request.POST['year'])
+        models.Car.objects.create(brand=brand,year=year)
+        return redirect(reverse('cars:list'))
+        #if user submitted new car--->then go to List.html
+    else:
+        return render(request,'cars/add.html')
+    
+
+def delete(request):
+    if request.POST:
+        #delete the car
+        pk = request.POST['pk']
+        try:
+            models.Car.objects.get(pk=pk).delete()
+            return redirect(reverse('cars:list'))
+        except:
+            print('pk not found!!!')
+            return render(request,'cars/delete.html')
+
+    else:
+        return render(request,'cars/delete.html')       
+    
